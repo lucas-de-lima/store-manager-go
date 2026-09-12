4baa957dac586d3c00f860ad46f05103ab38b0e3
lucas-de-lima <lucasramosdelima@gmail.com>
2026-09-12 19:42:15 -0300

    feat: resolve US-13 - GET /products/:id — Get product by ID (or 404)

---
+++ database/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO StoreManager.products (name) VALUES ("Martelo de Thor");
+INSERT INTO StoreManager.products (name) VALUES ("Traje de encolhimento");
+INSERT INTO StoreManager.products (name) VALUES ("Escudo do Capitão América");
